f462de435448f2951ce75f7f3097fe84b294fbc8
IbrokhimShukurullayev <ibrokhimjonshukurullayev>
2023-12-13 00:08:13 +0500

    commit

---
+++ js/main.js
@@ -0,0 +1,332 @@
+//  if1
+ 
+// let a = -5;
+
+// if (a > 0) {
+//   console.log(a + 1);
+// } else {
+//   console.log(a);
+// }
+
+// if2
+
+
+// let a = -10;
+
+// if (a > 0) {
+//   console.log(a + 1);
+// } else {
+//   console.log(a - 2);
+// }
+
+// if3
+
+// let a = -10;
+// let b = -8;
+// let c = 6;
+// let negativeCount = 0;
+
+
+// if (a > 0) {
+//   negativeCount++;
+// }
+
+// if (b > 0) {
+//   negativeCount++;
+// }
+
+// if (c > 0) {
+//   negativeCount++;
+// }
+
+// console.log(negativeCount);
+
+// if4
+
+// let a = 30;
+
+// let b = 20;
+
+// if (a > b) {
+//   console.log(a);
+// } else {
+//   console.log(b);
+// }
+
+// if5
+
+
+// let a = 10;
+
+// let b = 40;
+
+// if (a < b) {
+//   console.log(1);
+// } else {
+//   console.log(2);
+// }
+
+// if6
+
+// let a = 40;
+
+// let b = 20;
+
+// if (a > b) {
+//   console.log(a);
+//   console.log(b);
+// } else {
+//   console.log(b);
+//   console.log(a);
+// }
+
+
+// if 7
+
+// let a = 10;
+// let b = 20;
+
+// let k = a;
+
+// if ((a = b)) {
+//   console.log(a);
+// }
+
+// if ((b = k)) {
+//   console.log(b);
+// }
+
+// if10
+
+// let a = 30;
+// let b = 20;
+// let c = 10;
+
+
+// if (a > b) {
+//   console.log(a);
+// }
+
+// if (b > a) {
+//   console.log(b);
+// }
+
+// if (c > a) {
+//   console.log(c);
+// }
+
+// if11
+
+// let a = 30;
+// let b = 20;
+// let c = 10;
+
+
+// if (a > b) {
+//   console.log(b);
+// } else if (b > c) {
+//   console.log(b);
+// } 
+
+// if12
+
+
+// let a = 6;
+// let b = 8;
+// let c = 15;
+
+// let ab = a + b;
+// let ac = a + c;
+// let bc = b + c;
+
+// if (ab > ac && ab > bc) {
+//   console.log(ab);
+// } else if (ac > ab && ac > bc) {
+//   console.log(ac);
+// } else if (bc > ab && bc > ac) {
+//   console.log(bc);
+// }
+
+// if13
+
+// let a = 30;
+// let b = -20;
+// let c = 10;
+
+// if (a > 0) {
+//   console.log(-a);
+// } else {
+//   console.log(a);
+// }
+
+// if (b > 0) {
+//   console.log(-b);
+// } else {
+//   console.log(b);
+// }
+
+// if (c > 0) {
+//   console.log(-c);
+// } else {
+//   console.log(c);
+// }
+
+// if14
+
+
+// let a = 30;
+// let b = -20;
+// let c = 10;
+
+// if (a > 0) {
+//   console.log(2 * a);
+// } else {
+//   console.log(-a);
+// }
+
+// if (b > 0) {
+//   console.log(2 * b);
+// } else {
+//   console.log(-b);
+// }
+
+// if (c > 0) {
+//   console.log(2 * c);
+// } else {
+//   console.log(-c);
+// }
+
+// if17
+
+// let x = +prompt("Biror yil kiriting!");
+
+// if (x % 100 === 0 && x % 400 !== 0) {
+//   console.log("Yilda 365 kun bor");
+// } else if (x % 4 === 0) {
+//   console.log("Yilda 366 kun bor");
+// } else if (x <= 0) {
+//   console.log("Bunday yil yo'q");
+// } else {
+//   console.log("Yilda 365 kun bor");
+// };
+
+
+// case1
+
+// let x = 2;
+
+// if (x >= 5) {
+//   console.log("Zo'r");
+// } else if (x >= 4) {
+//   console.log("Yaxshi");
+// } else if (x >= 3) {
+//   console.log("qoniqarli");
+// } else if (x >= 2) {
+//   console.log("qoniqarsiz");
+// } else  {
+//   console.log("yomon");
+// }
+
+// case2
+
+// let day = 3;
+
+// if (day === 1) {
+//   console.log("Winter");
+// } else if (day === 2) {
+//   console.log("Spring");
+// } else if (day === 3) {
+//   console.log("Summer");
+// } else if (day === 4) {
+//   console.log("Autumn");
+// } else {
+//     console.log("no season");
+// }
+
+// case3
+
+// let day = 7;
+
+// if (day === 1) {
+//   console.log("yanvar");
+// } else if (day === 2) {
+//   console.log("fevral");
+// } else if (day === 3) {
+//   console.log("mart");
+// } else if (day === 4) {
+//   console.log("aprel");
+// } else if (day === 5) {
+//   console.log("may");
+// } else if (day === 6) {
+//   console.log("iyun");
+// } else if (day === 7) {
+//   console.log("iyul");
+// } else if (day === 8) {
+//   console.log("avgust");
+// } else if (day === 9) {
+//   console.log("sentabr");
+// } else if (day === 10) {
+//   console.log("oktabr");
+// } else if (day === 11) {
+//   console.log("noyabr");
+// } else if (day === 12) {
+//   console.log("dekabr");
+// } else  {
+//   console.log("No day");
+// }
+
+// case4
+
+
+// let x = +prompt("1 dan 5 gacha bo'lgan butun son kiriting!");
+// let y = +prompt("Uzunlikni kiriting!");
+
+// let res;
+
+// switch (x) {
+//   case 1:
+//     res = y + " detsimetr = " + y / 10 + " metr ga teng";
+//     break;
+//   case 2:
+//     res = y + " kilometr = " + y * 1000 + " metr ga teng";
+//     break;
+//   case 3:
+//     res = y + " metr" + y + " metr ga teng";
+//     break;
+//   case 4:
+//     res = y + " millimetr = " + y / 1000 + " metr ga teng";
+//     break;
+//   case 5:
+//     res = y + " santimetr = " + y / 100 + " metr ga teng";
+//     break;
+//   default:
+//     res = "Xato";
+//     break;
+// }
+
+// console.log(res);
+
+// case5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
